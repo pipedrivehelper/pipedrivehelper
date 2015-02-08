40ae1c13762d1a97709623369b62c0e38f63cc95
Nilpo <devnilpo@gmail.com>
2015-02-08 02:33:10 -0500

    crude working implementation

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-(function() {
+/*(function() {
   // receives message from popup script
   chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     if (request.opened == true) {
@@ -11,4 +11,142 @@
       });
     }
   });
-})();
+})();*/
+
+(function(w) {
+  // Idea based loosely on Lee Turner's GreaseMonkey script for Firefox
+  // http://blog.pipedrive.com/2015/01/pipedrive-users-tricks-and-hacks/
+  
+  var PipedriveHelper = function PipedriveHelper() {
+    
+    var self = this;
+    window.tabs = [];
+    
+    var urlRegex = /https:\/\/app.pipedrive.com/;
+    var startPage = 'https://app.pipedrive.com';
+    var baseUrl = 'https://app.pipedrive.com/';
+    
+    // set a couple of listeners to keep track of open Pipedrive tabs
+    
+    // watch for open tabs that navigate to Pipedrive
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+      isPipedriveTab(tab);
+    });
+    
+    // watch for new Pipedrive tabs
+    chrome.tabs.onCreated.addListener(function(tab) {
+      isPipedriveTab(tab);
+    });
+    
+    // monitor closed tabs and so we can stop watching them
+    chrome.tabs.onRemoved.addListener(function(tabId, info) {
+      ignoreTab(tabId);
+    });
+    
+    var isPipedriveTab = function isPipedriveTab(tab) {
+      if (tab.status !== "complete")
+        return;
+      if (urlRegex.test(tab.url)) {
+        // tab has a Pipedrive address
+        watchTab(tab);
+        return;
+      }
+      ignoreTab(tab.id);
+    };
+    
+    var watchTab = function watchTab(tab) {
+      tabs[tab.id] = tab.url;
+    };
+    
+    var ignoreTab = function ignoreTab(tabId) {
+      tabs.splice(tabId, 1);
+      // was that the last Pipedrive tab?
+      if (Object.keys(tabs).length < 1) {
+        // hide the badge
+        chrome.browserAction.setBadgeText({text:''});
+      }
+    };
+    
+    chrome.browserAction.onClicked.addListener(function (tab) {
+      //TODO: check for and activate any open tab first
+      chrome.tabs.create({
+        url: startPage
+      });
+    });
+    
+    // receives message from content script
+    chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+      if (request.action !== undefined) {
+        // sends response to popup script
+        //sendResponse({text: request.message+" OK"});
+
+        // sends response to content script
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {message: request.message+" OK"});
+        });
+        
+        // navigate to the desired location
+        var url = self.baseUrl + request.action;
+        self.navigateTo(url);
+      }
+      
+      if (request.activities !== undefined) {
+        // sends response to content script
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            message: "Activities ("+request.activities+") OK"
+          });
+        });
+        
+        chrome.browserAction.setBadgeText({
+          text: request.activities
+        });
+        
+        var color = (request.activities == "0") ? [0, 255, 0, 128] : [255, 0, 0, 128];
+        chrome.browserAction.setBadgeBackgroundColor({
+          color: color
+        });
+      }
+    });
+    
+    chrome.commands.onCommand.addListener(function(command) {
+      console.log('Command:', command);
+      
+      switch(command) {
+        
+        case "launch-in-current-tab":
+          console.log('Do launch-in-current-tab');
+          self.navigateTo(self.startPage);
+          break;
+          
+        default:
+          // do nothing;
+          
+      }
+    
+    });
+    
+    self.navigateTo = function navigateTo(url, tab) {
+      console.log('Navigate to:', url);
+      var whichTab = (tab !== undefined) ? tab.id : null;
+      // null represents the active tab
+      chrome.tabs.update(whichTab, {
+        url: url
+      })
+    };
+    
+    self.deduplicate = function(a) {
+      var n = 0, r = [];
+      for (var i = 0; i < a.length; i++)
+        n[a[i]] = true;
+      for (var k in t)
+        r.push(k);
+    
+      return r;
+    }
+    
+  }
+  
+  var ph = new PipedriveHelper();
+  
+})(window);
--- src/content.js
+++ src/content.js
@@ -1,7 +1,80 @@
-// receives message from background script
-chrome.extension.onMessage.addListener(function(message, sender) {
-  if (message.logUrl) {
-    var url = document.URL;
-    alert(url);
-  }
-});
+$(function(w){
+  
+  // receives message from background script
+  chrome.extension.onMessage.addListener(function(message, sender) {
+    if (message) {
+      console.log('message', message);
+    }
+  });
+  
+  if (w == top) {
+    w.addEventListener('keydown', function(e) {
+      var page;
+      console.log(e.keyCode);
+      if (e.keyCode == 68 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-d: goto deals pipeline view
+        page = "pipeline";
+      }
+      if (e.keyCode == 76 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-l: goto deals list view
+        page = "deal";
+      }
+      if (e.keyCode == 84 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-t: goto deals timeline view
+        page = "timeline";
+      }
+      if (e.keyCode == 77 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-m: goto mail
+        page = "mailbox";
+      }
+      if (e.keyCode == 65 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-a: goto activities
+        page = "activities";
+      }
+      if (e.keyCode == 80 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-p: goto people
+        page = "people";
+      }
+      if (e.keyCode == 79 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-o: goto organizations
+        page = "org"
+      }
+      if (e.keyCode == 83 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-s: goto statistics
+        page = "company/details"
+      }
+      if (e.keyCode == 88 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+        // alt-x: goto settings
+        page = "settings"
+      }
+      if (e.keyCode == 191 && !e.shiftKey && !e.ctrlKey && !e.altKey && !e.metaKey) {
+        // pressed forward slash for search – /
+        e.preventDefault();
+        document.getElementsByName("needle")[0].focus();
+        return;
+      }
+    
+      // sends message to background script
+      chrome.runtime.sendMessage({action: page}, function(response) {
+        console.log(response.message);
+      });
+      return;
+    }, false);
+  };
+  
+  
+  // Pipedrive uses a lot of dynamically loaded elements.  We'll need to get tricky
+  // to grab DOM elements when they are actually available.
+  var loaded = false;
+  $(document).bind("DOMSubtreeModified", function() {
+    // is the menu loaded yet?
+    var active = $("li.key-activities a span.count").text();
+    
+    var count = (active.length > 0) ? active : "0";
+    // sends message to background script
+    chrome.runtime.sendMessage({activities: count}, function(response) {
+      console.log("response.message", response.message);
+    });
+  });
+
+}(window));
