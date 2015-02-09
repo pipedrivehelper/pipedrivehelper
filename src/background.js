2b2fb97f5e77a2fdd2e6e25c0ed6ae395ded6428
Nilpo <devnilpo@gmail.com>
2015-02-09 01:53:34 -0500

    refactoring codebase

--- src/background.js
+++ src/background.js
@@ -1,155 +1,160 @@
-/*(function() {
-  // receives message from popup script
-  chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-    if (request.opened == true) {
-      // sends response back to popup script
-      sendResponse({example: "goodbye"});
-
-      // sends response to content script
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { logUrl: true } );
-      });
-    }
-  });
-})();*/
-
-
 // TODO:
-//    Cleanup code and refactor
 //    Add Alt+H combo with help dialog
 //    Add custom urls (Alt+1, Alt+2, etc.)
 //    Add settings page to manage features
 //    Add option to choose badget text (new mail, activites, etc.)
 
-chrome.commands.getAll(function(commands) {
-  console.debug(commands);
-});
-
-(function(w) {
-  // Idea based loosely on Lee Turner's GreaseMonkey script for Firefox
-  // http://blog.pipedrive.com/2015/01/pipedrive-users-tricks-and-hacks/
-  
-  var PipedriveHelper = function PipedriveHelper() {
+(function() {
     
-    var self = this;
-    tabs = [];
+    "use strict";
     
-    var urlRegex = /https:\/\/app.pipedrive.com/;
-    var startPage = 'https://app.pipedrive.com';
-    var baseUrl = 'https://app.pipedrive.com/';
-    
-    // set a couple of listeners to keep track of open Pipedrive tabs
-    
-    // watch for open tabs that navigate to Pipedrive
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-      isPipedriveTab(tab);
-    });
-    
-    // watch for new Pipedrive tabs
-    chrome.tabs.onCreated.addListener(function(tab) {
-      isPipedriveTab(tab);
-    });
-    
-    // monitor closed tabs and so we can stop watching them
-    chrome.tabs.onRemoved.addListener(function(tabId, info) {
-      ignoreTab(tabId);
-    });
-    
-    var isPipedriveTab = function isPipedriveTab(tab) {
-      if (tab.status !== "complete")
-        return;
-      if (urlRegex.test(tab.url)) {
-        // tab has a Pipedrive address
-        watchTab(tab);
-        return;
-      }
-      ignoreTab(tab.id);
+    var PipedriveHelper = {
+        
+        init: function() {
+            
+            this._tabs = [];
+            this._urlRegex  = /https:\/\/app.pipedrive.com/;
+            this._startPage = 'https://app.pipedrive.com';
+            this._baseUrl   = 'https://app.pipedrive.com/';
+            
+            this._addListeners();
+            
+            return this;
+            
+        },
+        
+        // set a couple of listeners to keep track of open Pipedrive tabs
+        _addListeners: function() {
+            
+            var self = this;
+            
+            // watch for open tabs that navigate to Pipedrive
+            chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+                self._isPipedriveTab(tab);
+            });
+            
+            // watch for new Pipedrive tabs
+            chrome.tabs.onCreated.addListener(function(tab) {
+                self._isPipedriveTab(tab);
+            });
+            
+            // monitor closed tabs and so we can stop watching them
+            chrome.tabs.onRemoved.addListener(function(tabId, info) {
+                self._ignoreTab(tabId);
+            });
+            
+            // listen for clicks to the toolbar icon
+            chrome.browserAction.onClicked.addListener(function (tab) {
+                //TODO: check for and activate any open tab first
+                chrome.tabs.create({
+                    url: self._startPage
+                });
+            });
+            
+            // listen for messages from content script
+            chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+                // handle registered keyboard shorcuts (defined in content script)
+                if (request.action !== undefined) {
+                    self._performAction(request);
+                }
+                
+                // handle updates to activities count
+                if (request.activities !== undefined) {
+                    self._updateActivityCount(request);
+                }
+            });
+            
+            // listen for command shortcuts (defined in manifest.json)
+            chrome.commands.onCommand.addListener(function(command) {
+                self._executeCommand(command);
+            });
+            
+        },
+        
+        _isPipedriveTab: function(tab) {
+            
+            if (tab.status !== "complete")
+                return;
+            
+            if (this._urlRegex.test(tab.url)) {
+                // tab has a Pipedrive address
+                this._watchTab(tab);
+                return;
+            }
+            this._ignoreTab(tab.id);
+            
+        },
+        
+        _watchTab: function(tab) {
+            
+            this._tabs[tab.id] = tab.url;
+            
+        },
+        
+        _ignoreTab: function(tabId) {
+            
+            this._tabs.splice(tabId, 1);
+            // was that the last Pipedrive tab?
+            if (Object.keys(this._tabs).length < 1) {
+                // hide the badge
+                chrome.browserAction.setBadgeText({text:''});
+            }
+            
+        },
+        
+        _performAction: function(request) {
+            
+            // send response to content script
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {message: request.message+" OK"});
+            });
+            
+            // navigate to the desired location
+            this._navigate(this._baseUrl + request.action);
+            
+        },
+            
+        _updateActivityCount: function(request) {
+            
+            // send response to content script
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    message: "Activities ("+request.activities+") OK"
+                });
+            });
+            
+            var color = (request.activities == "0") ? [0, 255, 0, 128] : [255, 0, 0, 128];
+            chrome.browserAction.setBadgeBackgroundColor({color: color});
+            chrome.browserAction.setBadgeText({text: request.activities});
+            
+        },
+        
+        _executeCommand: function(command) {
+            
+            switch(command) {
+                
+                case "launch-in-current-tab":
+                    console.log('Do launch-in-current-tab');
+                    self.navigateTo(startPage);
+                    break;
+                    
+                default:
+                    // do nothing;
+                    
+            }
+        
+        },
+        
+        _navigateTo: function(url, tab) {
+            
+            var whichTab = (tab !== undefined) ? tab.id : null;
+            // null represents the active tab
+            chrome.tabs.update(whichTab, {url: url})
+            
+        }
+        
     };
     
-    var watchTab = function watchTab(tab) {
-      tabs[tab.id] = tab.url;
-    };
+    var ph = PipedriveHelper.init();
     
-    var ignoreTab = function ignoreTab(tabId) {
-      tabs.splice(tabId, 1);
-      // was that the last Pipedrive tab?
-      if (Object.keys(tabs).length < 1) {
-        // hide the badge
-        chrome.browserAction.setBadgeText({text:''});
-      }
-    };
-    
-    chrome.browserAction.onClicked.addListener(function (tab) {
-      //TODO: check for and activate any open tab first
-      chrome.tabs.create({
-        url: startPage
-      });
-    });
-    
-    // receives message from content script
-    chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-      if (request.action !== undefined) {
-        // sends response to popup script
-        //sendResponse({text: request.message+" OK"});
-
-        // sends response to content script
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {message: request.message+" OK"});
-        });
-        
-        // navigate to the desired location
-        console.log(baseUrl);
-        var url = baseUrl + request.action;
-        self.navigateTo(url);
-      }
-      
-      if (request.activities !== undefined) {
-        // sends response to content script
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {
-            message: "Activities ("+request.activities+") OK"
-          });
-        });
-        
-        chrome.browserAction.setBadgeText({
-          text: request.activities
-        });
-        
-        var color = (request.activities == "0") ? [0, 255, 0, 128] : [255, 0, 0, 128];
-        chrome.browserAction.setBadgeBackgroundColor({
-          color: color
-        });
-      }
-    });
-    
-    chrome.commands.onCommand.addListener(function(command) {
-      console.log('Command:', command);
-      
-      switch(command) {
-        
-        case "launch-in-current-tab":
-          console.log('Do launch-in-current-tab');
-          self.navigateTo(startPage);
-          break;
-          
-        default:
-          // do nothing;
-          
-      }
-    
-    });
-    
-    self.navigateTo = function navigateTo(url, tab) {
-      console.log('Navigate to:', url);
-      var whichTab = (tab !== undefined) ? tab.id : null;
-      // null represents the active tab
-      chrome.tabs.update(whichTab, {
-        url: url
-      })
-    };
-    
-  }
-  
-  var ph = new PipedriveHelper();
-  
-})(window);
+})();
--- src/content.js
+++ src/content.js
@@ -1,80 +1,105 @@
 $(function(w, d){
-  
-  // receives message from background script
-  chrome.extension.onMessage.addListener(function(message, sender) {
-    if (message) {
-      console.log('message', message);
-    }
-  });
-  
-  if (w == top) {
-    w.addEventListener('keydown', function(e) {
-      var page;
-      console.log(e.keyCode);
-      if (e.keyCode == 68 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-d: goto deals pipeline view
-        page = "pipeline";
-      }
-      if (e.keyCode == 76 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-l: goto deals list view
-        page = "deal";
-      }
-      if (e.keyCode == 84 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-t: goto deals timeline view
-        page = "timeline";
-      }
-      if (e.keyCode == 77 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-m: goto mail
-        page = "mailbox";
-      }
-      if (e.keyCode == 65 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-a: goto activities
-        page = "activities";
-      }
-      if (e.keyCode == 80 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-p: goto people
-        page = "people";
-      }
-      if (e.keyCode == 79 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-o: goto organizations
-        page = "org"
-      }
-      if (e.keyCode == 83 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-s: goto statistics
-        page = "company/details"
-      }
-      if (e.keyCode == 88 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-        // alt-x: goto settings
-        page = "settings"
-      }
-      if (e.keyCode == 191 && !e.shiftKey && !e.ctrlKey && !e.altKey && !e.metaKey) {
-        // pressed forward slash for search – /
-        e.preventDefault();
-        d.getElementsByName("needle")[0].focus();
-        return;
-      }
     
-      // sends message to background script
-      chrome.runtime.sendMessage({action: page}, function(response) {
-        console.log(response.message);
-      });
-      return;
-    }, false);
-  };
-  
-  
-  // Pipedrive uses a lot of dynamically loaded elements.  We'll need to get tricky
-  // to grab DOM elements when they are actually available.
-  var loaded = false;
-  $(d).bind("DOMSubtreeModified", function() {
-    // is the menu loaded yet?
-    var active = $("li.key-activities a span.count").text();
+    // Inspiration from Lee Turner's GreaseMonkey script for Firefox
+    // http://blog.pipedrive.com/2015/01/pipedrive-users-tricks-and-hacks/
     
-    var count = (active.length > 0) ? active : "0";
-    // sends message to background script
-    chrome.runtime.sendMessage({activities: count}, function(response) {
-      console.log("response.message", response.message);
+    "use strict";
+    
+    // listen for messages from background script
+    chrome.extension.onMessage.addListener(function(message, sender) {
+        if (message) {
+            // In production, do nothing.
+            //console.log('message', message);
+        }
     });
-  });
+    
+    // listen for keyboard shortcuts and dispatch notifications to the background script
+    if (w == top) {
+        w.addEventListener('keydown', function(e) {
+            var page = null;
+            
+            // alt-d: goto deals pipeline view
+            if (e.keyCode == 68 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "pipeline";
+            }
+            
+            // alt-l: goto deals list view
+            if (e.keyCode == 76 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "deal";
+            }
+            
+            // alt-t: goto deals timeline view
+            if (e.keyCode == 84 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "timeline";
+            }
+            
+            // alt-m: goto mail
+            if (e.keyCode == 77 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "mailbox";
+            }
+            
+            // alt-a: goto activities
+            if (e.keyCode == 65 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "activities";
+            }
+            
+            // alt-p: goto people
+            if (e.keyCode == 80 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "people";
+            }
+            
+            // alt-o: goto organizations
+            if (e.keyCode == 79 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "org"
+            }
+            
+            // alt-s: goto statistics
+            if (e.keyCode == 83 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "company/details"
+            }
+            
+            // alt-x: goto settings
+            if (e.keyCode == 88 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                page = "settings"
+            }
+            
+            // alt-h: show modal help dialog
+            if (e.keyCode == 88 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+                //TODO: implement a modal help dialog with shorcuts
+            }
+            
+            // pressed forward slash for search – /
+            if (e.keyCode == 191 && !e.shiftKey && !e.ctrlKey && !e.altKey && !e.metaKey) {
+                e.preventDefault();
+                d.getElementsByName("needle")[0].focus();
+                return;
+            }
+        
+            // sends message to background script with action
+            chrome.runtime.sendMessage({action: page}, function(response) {
+                // In production, do nothing
+                //console.log(response.message);
+            });
+            return;
+            
+        }, false);
+    };
+    
+    // Get the current activity count.
+    // Pipedrive uses a lot of dynamically loaded elements.    We'll need to get tricky
+    // to grab DOM elements when they are actually available.
+    // 
+    // Listen for changes to the DOM tree
+    $(d).bind("DOMSubtreeModified", function() {
+        //TODO: Implement native JS element selector and remove jQuery dependency
+        // is the menu loaded yet?
+        var active = $("li.key-activities a span.count").text();
+        var count = (active.length > 0) ? active : "0";
+        
+        // notify the background script
+        chrome.runtime.sendMessage({activities: count}, function(response) {
+            console.log("response.message", response.message);
+        });
+    });
 
 }(window, document));
